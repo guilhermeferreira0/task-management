e950f7f77ca1f1be4ba3de882db301c250390cee
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-08-24 15:58:07 -0300

    feat: adding crud for users

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,14 +1,91 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { type Request, type Response } from 'express';
+import { NextFunction, type Request, type Response } from 'express';
+import { User } from '../models/userModel';
 
 export class UserController {
-  getAllUsers(req: Request, res: Response) {}
+  // get all users for database
+  async getAllUsers(req: Request, res: Response, next: NextFunction) {
+    try {
+      const users = await User.findAll();
+      return res.status(200).json({ success: true, data: users });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Failed getAllUsers' });
+    }
+  }
 
-  getUser(req: Request, res: Response) {}
+  // get specific user for id
+  async getUser(req: Request, res: Response) {
+    const userId = req.params.id;
+    try {
+      const userExisting = await User.findByPk(userId);
+      if (!userExisting) {
+        return res
+          .status(406)
+          .json({ success: false, message: 'User is undefined' });
+      }
+      return res.status(200).json({ success: true, data: userExisting });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Failed getUser' });
+    }
+  }
 
-  register(req: Request, res: Response) {}
+  // register new user
+  async register(req: Request, res: Response) {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res
+        .status(406)
+        .json({ success: false, message: 'Email or password Undefined' });
+    }
+    try {
+      const newUser = await User.create({ username, email, password });
+      return res.status(201).json({ success: true, data: newUser });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Failed getAllUsers' });
+    }
+  }
 
-  delete(req: Request, res: Response) {}
+  // delete specific user for id
+  async delete(req: Request, res: Response) {
+    const userId = req.params.id;
+    try {
+      const userExisting = await User.findByPk(userId);
+      if (!userExisting) {
+        return res
+          .status(406)
+          .json({ success: false, message: 'User is undefined' });
+      }
+      await userExisting.destroy();
+      return res.status(200).json({ success: true, message: 'User deleted' });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Failed getUser' });
+    }
+  }
 
-  update(req: Request, res: Response) {}
+  // update specific user for id
+  async update(req: Request, res: Response) {
+    const userId = req.params.id;
+    const { username, email, password } = req.body;
+    try {
+      const userExisting = await User.findByPk(userId);
+      if (!userExisting) {
+        return res
+          .status(406)
+          .json({ success: false, message: 'User is undefined' });
+      }
+      const userUpdated = await userExisting.update({ username, email, password });
+      return res.status(200).json({ success: true, data: userUpdated });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Failed getUser' });
+    }
+  }
 }
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -4,8 +4,8 @@ import { UserController } from '../controllers/userController';
 export const userRouter = Router();
 const user = new UserController();
 
-userRouter.delete('/update', user.delete);
-userRouter.put('/update', user.update);
+userRouter.delete('/delete/:id', user.delete);
+userRouter.put('/update/:id', user.update);
 userRouter.post('/register', user.register);
-userRouter.get('/', user.getAllUsers);
 userRouter.get('/:id', user.getUser);
+userRouter.get('/', user.getAllUsers);
