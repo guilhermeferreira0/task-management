400defe4c70c54c7c1db9d0b53d7c05532cd8fb6
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-08-24 15:56:55 -0300

    feat: adding new rule no-used-vars

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import jest from 'eslint-plugin-jest';
+import jest, { rules } from 'eslint-plugin-jest';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
@@ -19,6 +19,11 @@ export default [
       ...jest.configs['flat/recommended'].rules,
       'jest/prefer-expect-assertions': 'off',
     },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+    }
   },
   eslintPluginPrettierRecommended,
 ];
