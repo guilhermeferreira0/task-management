bedf66c5962ae73f51eaa8edfc8d35e4c9a24a5a
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-08-26 11:30:40 -0300

    test: verify funct auth

---
+++ backend/src/middlewares/__test__/auth.spec.ts
@@ -0,0 +1,16 @@
+import { authMiddleware } from '../auth';
+import AuthService from '../../services/auth';
+
+describe('Auth Middleware', () => {
+  it('Should verify a JWT token and call the next middleware', async () => {
+    const jwtToken = AuthService.generateToken('fake-user-id', 'fake-user-email');
+    const reqFake = {
+      headers: {
+        authorization: jwtToken
+      }
+    }
+    const resFake = {};
+    const nextFake = jest.fn();
+    authMiddleware(reqFake, resFake, nextFake);
+    expect(nextFake).not.toHaveBeenCalled();
+})});
