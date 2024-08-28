c7d77492070b804e6d0ecbda57bea38cfa02c745
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-08-28 08:34:54 -0300

    feat: adding protected route component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { MainPage } from './components/MainPage';
 import { FormPage } from './components/Login';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App(): JSX.Element {
   return (
     <>
       <Routes>
         <Route path="/" element={<FormPage />} />
-        <Route path="/dashboard" element={<MainPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <MainPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
---
+++ frontend/src/components/ProtectedRoute/index.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { userDetailsRequest } from '../../api/user';
+import { useAuth } from '../../contexts/AuthContext/useAuth';
+import { setUserLocalStorage } from '../../contexts/AuthContext/util';
+import { useNavigate } from 'react-router-dom';
+
+interface ProtectedRouteProps {
+  children: JSX.Element;
+}
+
+export function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const { logout, setUserLogged } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function verifyUser() {
+      try {
+        const res = await userDetailsRequest();
+        if (!res) {
+          logout();
+          navigate('/');
+          return;
+        }
+        setUserLogged(res.data);
+        setUserLocalStorage(res.data);
+      } catch (e) {
+        logout();
+        navigate('/');
+      }
+    }
+    verifyUser();
+
+    return;
+  }, []);
+
+  return <>{children}</>;
+}
