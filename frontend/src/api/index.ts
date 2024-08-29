e211611affb6521df4f68247186adf3ce145704f
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-08-29 09:11:07 -0300

    chore: adding dotenv for protected api baseurl

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+if (!process.env.REACT_APP_API_BASEURL)
+  throw new Error('DOTENV axios BASEURL undefined');
+
 export const Api = axios.create({
-  baseURL: 'http://localhost:3333/api',
+  baseURL: process.env.REACT_APP_API_BASEURL as string,
 });
