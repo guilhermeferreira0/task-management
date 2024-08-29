d9a85f40b91eb5b9289ac3052db8efe5efa1158a
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-08-29 08:52:38 -0300

    refactor: configuring connection to use dotenv

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,10 +1,18 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
+if (!process.env.MYSQL_DATABASE) throw new Error('DOTENV Mysql database undefined');
+if (!process.env.MYSQL_USERNAME) throw new Error('DOTENV Mysql username undefined');
+if (!process.env.MYSQL_PASSWORD) throw new Error('DOTENV Mysql password undefined');
+if (!process.env.MYSQL_HOST) throw new Error('DOTENV Mysql host undefined');
+
 class Connection {
-  private readonly connection = new Sequelize('task', 'root', 'root', {
+  private readonly connection = new Sequelize(
+    process.env.MYSQL_DATABASE as string, 
+    process.env.MYSQL_USERNAME as string, 
+    process.env.MYSQL_PASSWORD as string, {
     dialect: 'mysql',
-    host: 'localhost',
+    host: process.env.MYSQL_HOST as string,
   });
 
   start() {
