2d6e4e87956d7400f2edbe13e6d8d3f272aa252e
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-09-07 07:57:43 -0300

    refactor: improving protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { MainPage } from './components/MainPage';
 import { FormPage } from './components/Login';
-import { ProtectedRoute } from './components/ProtectedRoute';
 import { Toast } from './components/Toasts';
 import 'react-toastify/dist/ReactToastify.css';
 import { HomePage } from './components/HomePage';
+import { useAuth } from './contexts/AuthContext/useAuth';
 
 function App(): JSX.Element {
+  const { userLogged } = useAuth();
+
   return (
     <>
-      <Toast />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<FormPage />} />
+        <Route
+          path="/"
+          element={userLogged ? <Navigate to="/dashboard" /> : <HomePage />}
+        />
+        <Route
+          path="/login"
+          element={userLogged ? <Navigate to="/dashboard" /> : <FormPage />}
+        />
         <Route
           path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <MainPage />
-            </ProtectedRoute>
-          }
+          element={userLogged ? <MainPage /> : <Navigate to="/login" />}
         />
       </Routes>
+      <Toast />
     </>
   );
 }
